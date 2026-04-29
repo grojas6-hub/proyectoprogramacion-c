a97b7d80e83a933f237542ddc796a113caddd22b
Josue <jvargas59@ucol.mx>
2026-04-29 09:48:24 -0600

    AES

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main() {
     inicio final 
     printf("Proyecto iniciado\n");
     esto solo es una practica de como hacer un cambio 
+    PERO OTRO NO SE GUARDO A VI 
     return 0;
 
         
